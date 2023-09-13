1340a7b1d8eb10ff4693cf69fc8dcbd914f11a37
david saffo <saffo.d@husky.neu.edu>
2023-09-13 11:19:25 -0400

    card

--- src/components/paperCard.svelte
+++ src/components/paperCard.svelte
@@ -1,39 +1,21 @@
 <script>
-
-	import {
-		Content,
-		PrimaryAction,
-		Media,
-		MediaContent,
-		Actions,
-		ActionButtons,
-		ActionIcons
-	} from '@smui/card';
-	import Snackbar, {  Label } from '@smui/snackbar';
-	import IconButton from '@smui/icon-button';
-
-	import  { Icon } from '@smui/button';
-	import List, { Item, Separator, Text } from '@smui/list';
 	import Clipboard from 'svelte-clipboard';
-	
+
+	import PaperDetail from './paperDetail.svelte';
+
     import { Wrapper } from '@smui/tooltip';
-    import Chip, { Set} from '@smui/chips';
     import { urlParams } from '../urlParamStore';
-
 	import { Card, Button, Toggle, Badge, Modal } from 'flowbite-svelte';
-  import { ArrowRightOutline, LinkSolid, CopySolid, LockOpenSolid, LockSolid} from 'flowbite-svelte-icons';
-  import { tree } from 'd3';
-  import { afterUpdate, beforeUpdate, onMount } from 'svelte';
-    import PaperDetail from './paperDetail.svelte';
-
+	import {  LinkSolid, CopySolid, LockOpenSolid, LockSolid} from 'flowbite-svelte-icons';
+	import {onMount } from 'svelte';
+    import { Media } from '@smui/card';
 	
 
 	export let paper;
 	export let summaryView;
 	export let detailView;
 	export let meta;
-	
-
+	let menu;
 	let snackbarWithClose;
 
 	// let urlParams = new URLSearchParams(window.location.search); 
@@ -41,7 +23,6 @@
 	urlParams.subscribe((value) => {
 		params = value;
 	});
-
     
 	
 	
@@ -51,7 +32,6 @@
 	
 	let modalState = false;
 	
-
 	onMount(() => {
 		if (params.get('paper') == paper.DOI){
 			modalState = true;
@@ -66,10 +46,18 @@
 	} 
 		
 </script>
-	
-	<Modal size='lg' bind:this={modal} id='modal' title="Terms of Service" bind:open={modalState} outsideclose>
+
+
+
+
+	<Modal bind:this={modal} size='lg' id='modal' title="Terms of Service" bind:open={modalState} outsideclose>
 		<PaperDetail {paper} {detailView} {meta} />
+	<!-- <svelte:fragment slot="footer">
+			<Button on:click={() => alert('Handle "success"')}>I accept</Button>
+			<Button color="alternative">Decline</Button>
+		</svelte:fragment> -->
 	</Modal>
+
 
 	<Card on:click={() => {modalState = true; window.history.replaceState(null, null, '?paper=' + paper.DOI);}} class="w-full m-1 relative" padding="none">
 	<Media class="card-media-16x9" aspectRatio="16x9" style="background-image: url(/images/{paper.img}); height: 200px" />
@@ -78,112 +66,36 @@
 		<div style="position: absolute; bottom: 0; width: 100%; display:flex; justify-content: space-between; align-items: center;">
 			<div>
 				<Badge class='m-1' color='dark' rounded>{paper.Opportunity}</Badge>
-				<Badge class='m-1' color='dark' rounded>{paper.Contribution}</Badge>
+				<Badge class='m-1' color='dark' rounded>{paper["Contribution Type"]}</Badge>
 			</div>
 			<div>
-				<Button on:click={() => window.open('https://doi.org/' + paper.DOI)} outline={true} size='xs' pill={true} class="!p-2 mb-2 mr-0.5 border-0"><LinkSolid class="w-4 h-4" /></Button>
+				<Button on:click={(event) => {event.stopPropagation(); window.open('https://doi.org/' + paper.DOI);}} outline={true} size='xs' pill={true} class="!p-2 mb-2 mr-0.5 border-0"><LinkSolid class="w-4 h-4" /></Button>
 				{#if paper["Open Access"] == "na"}
 				<Button disabled outline={true} size='xs' pill={true} class="!p-2 mb-2  mr-0.5 border-0"><LockSolid class="w-4 h-4" /></Button>
 				{:else}
-				<Button on:click={() => window.open(paper['Open Access'])} outline={true} size='xs' pill={true} class="!p-2 mb-2  mr-0.5 border-0"><LockOpenSolid class="w-4 h-4" /></Button>
+				<Button on:click={(event) => {event.stopPropagation(); window.open(paper['Open Access']);}} outline={true} size='xs' pill={true} class="!p-2 mb-2  mr-0.5 border-0"><LockOpenSolid class="w-4 h-4" /></Button>
 				{/if}
-				<Wrapper>
 					<Clipboard
 					text={paper.Bibtex}
 					let:copy
 					on:copy={() => {
 						snackbarWithClose.open();
 					}}>
-					<Button on:click={copy} outline={true} size='xs' pill={true} class="!p-2 mb-2  mr-1 border-0"><CopySolid class="w-4 h-4" /></Button>
+					
+					<Button on:click={(event) => {event.stopPropagation(); copy;}} outline={true} size='xs' pill={true} class="!p-2 mb-2  mr-1 border-0"><CopySolid class="w-4 h-4" /></Button>
 				</Clipboard>
-				</Wrapper>
+			
 			</div>
     	</div>
-	
-
 	</Card>
-
+	
 
 
 			 
 	
-	<!-- <Card class={summaryView.view === "image" ? 'card-image' : 'card-text'} style="width: 400px; height: 400px; flex-grow: 1; margin: 10px">
-		<PrimaryAction on:click={() => (open = true)}>
-			{#if summaryView.showImg} -->
-				<!-- <Media class="card-media-square" aspectRatio="square" src={paper.img ? paper.img : "/images/defaultImage.png"} /> -->
-			<!-- {/if}
-			<Content class="mdc-typography--body5" style={"display:flex"}>
-				{#if summaryView.showImg}
-					<div style={"padding-right:10px"}>
-							<img src={paper.image ? paper.image : "/images/defaultImage.png"} 
-									width="200" height=200
-									alt={paper.altImage ? paper.altImage: "Image from the paper"}
-									/>
-					</div>
-				{/if}
-				<div>
-					<h2 class="mdc-typography--headline6" style="margin: 0;">{paper.Name} ({paper.Year})</h2>
-					<h3 class="mdc-typography--subtitle2" style="margin: 0 0 10px; color: #888;">
-						by {paper.Authors}
-					</h3>
-					{#each summaryView.show as prop}
-						{#if meta[prop].type === 'String'}
-							<div class="string-select">
-								<div class="mdc-typography--body1"><strong>{prop}:&nbsp;</strong></div>
-								<div class="mdc-typography--body1">{paper[prop]}</div>
-							</div>
-						{:else if meta[prop].type === 'MultiSelect'}
-							<div class="multi-select">
-								<div class="mdc-typography--body1"><strong>{prop}:</strong></div>
-								{#if paper[prop].length > 0 && paper[prop][0] !== ''}
-									<Multiselect list={paper[prop]} />
-								{/if}
-							</div>
-						{/if}
-					{/each}
-				</div>
-			</Content>
-		</PrimaryAction>
-		<div style={"z-index:2;"}>
-			<Actions style="padding-left:15px"> -->
-				<!-- Added by: Tarik Crnovrsanin. -->
-				<!-- <ActionButtons> -->
-					<!-- <Button on:click={() => clicked++}>
-						<Label>new Window</Label>
-					</Button> -->
-				<!-- </ActionButtons>
-				<ActionIcons>
-					<div class="menu-loc">
-						<IconButton class="material-icons" 
-							on:click={() => menu.setOpen(true)} title="More options"
-							>more_vert</IconButton
-						>
-						<Menu bind:this={menu}>
-							<List>
-								<Clipboard
-											text={paper.Bibtex}
-											let:copy
-											on:copy={() => {
-												snackbarWithClose.open();
-											}}
-										>
-									<Item on:SMUI:action={copy}>
-									<Icon class="material-icons">download</Icon>
-									<Text>BibTex</Text>
-									</Item>
-								</Clipboard>
-								<Item on:SMUI:action={() => console.log("work in progress")}>
-									<Text>Update Entry</Text>
-								</Item>
-							</List>
-						</Menu>
-					</div>
-				</ActionIcons>
-			</Actions>
-		</div>
-	</Card> --> 
 
 <style>
+	
 	.multi-select {
 		display: flex;
 		align-items: center;
@@ -208,3 +120,4 @@
     	background-image: url(https://placehold.co/320x180?text=16x9);
   	}
 </style>
+
